aca52ef19125e544eb065c5f1a4a93de20a89aad
André Paulovich <andrepg@ciandt.com>
2016-02-27 17:40:57 -0300

    refactor menus - codigo centralizado

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,2 +1,21 @@
 var API_URL = "https://include-day-3.appspot.com/_ah/api";
 var API_CHAT_URL = "http://chat-site.azurewebsites.net/signalr";
+var MENU = {
+    links:[
+    {
+        nome: "Times",
+        url: "times.html",
+    },
+    {
+        nome: "Tipos de incidentes",
+        url: "tipos.html",
+    },
+    {
+        nome: "Incidentes",
+        url: "incidentes.html",
+    },
+    {
+        nome: "Chat",
+        url: "chat.html",
+    },
+]};
--- Frontend/js/timesCtrl.js
+++ Frontend/js/timesCtrl.js
@@ -1,7 +1,11 @@
-var Tipos = function () {
+var Times = function () {
+
+    var limparLista = function () {
+        $('.table-result').empty();
+    };
 
     var limparFormulario = function () {
-        $('#id-tipo').val('');
+        $('#id-time').val('');
         $('#tipo').val('');
     };
 
@@ -12,12 +16,12 @@ var Tipos = function () {
         $.ajax({
             async: true,
             type: "GET",
-            url: API_URL + '/tipoincidente/v1/tipoincidente/' + $id,
+            url: API_URL + '/times/v1/times/' + $id,
             dataType: "json",
             contentType: "application/json; charset=utf-8",
             success: function (data) {
                 $('#tipo').val(data.descricao);
-                $('#id-tipo').val(data.id);                
+                $('#id-time').val(data.id);
             },
             error: function (xhr) {
                 bootbox.alert(xhr.responseJSON.error.message);
@@ -31,15 +35,15 @@ var Tipos = function () {
             descricao: $('#tipo').val()
         };
 
-        if ($('#id-tipo').val() != '') {
-            item.id = $('#id-tipo').val();
+        if ($('#id-time').val() != '') {
+            item.id = $('#id-time').val();
         };
 
         $.ajax({
             async: true,
             type: "POST",
             data: JSON.stringify(item),
-            url: API_URL + '/tipoincidente/v1/tipoincidente?alt=json',
+            url: API_URL + '/times/v1/times?alt=json',
             dataType: "json",
             contentType: "application/json; charset=utf-8",
             success: function (data) {
@@ -47,8 +51,8 @@ var Tipos = function () {
                 carregarLista();
                 bootbox.alert('Cadastrado/Editado com sucesso!');
             },
-            error: function (xhr) {                
-                bootbox.alert(xhr.responseJSON.error.message);                
+            error: function (xhr) {
+                bootbox.alert(xhr.responseJSON.error.message);
             }
         });
 
@@ -56,7 +60,7 @@ var Tipos = function () {
 
     var removerItem = function () {
         var $this = $(this);
-        var $id = $this.attr('id-objeto');
+        var $id = $this.attr('id-time');
 
         bootbox.confirm("Tem certeza que deseja remover?", function (result) {
 
@@ -64,7 +68,7 @@ var Tipos = function () {
                 $.ajax({
                     async: true,
                     type: "DELETE",
-                    url: API_URL + '/tipoincidente/v1/tipoincidente/' + $id,
+                    url: API_URL + '/times/v1/times/' + $id,
                     dataType: "JSON",
                     processData: true,
                     success: function (data) {
@@ -72,7 +76,7 @@ var Tipos = function () {
                         bootbox.alert('Removido com sucesso!');
                     },
                     error: function (xhr) {
-                        bootbox.alert(xhr.responseJSON.error.message);                        
+                        bootbox.alert(xhr.responseJSON.error.message);
                     }
                 });
             }
@@ -99,32 +103,34 @@ var Tipos = function () {
         $.ajax({
             async: true,
             type: "GET",
-            url: API_URL + '/tipoincidente/v1/tipoincidente/',
+            url: API_URL + '/times/v1/times/',
             dataType: "JSON",
             processData: true,
             success: function (data) {
 
-                Utils.limparLista();
+                if (data != null) {
 
-                $.each(data.items, function (i, item) {
-                    var tr = $('<tr/>');
-                    tr.append("<td>" + item.id + "</td>");
-                    tr.append("<td>" + item.descricao + "</td>");
+                    $.each(data.items, function (i, item) {
+                        var tr = $('<tr/>');
+                        tr.append("<td>" + item.id + "</td>");
+                        tr.append("<td>" + item.nome + "</td>");
+                        tr.append("<td>" + item.integrantes + "</td>");
+                        tr.append("<td>" + item.cidade + "</td>");
 
-                    var template = "<td>";
-                    template += "<div class='btn-group btn-group-xs'>";
-                    template += "<button type='button' id-objeto='" + item.id + "' class='btn btn-default'><span class='glyphicon glyphicon glyphicon-pencil' aria-hidden='true'></span></button>";
-                    template += "<button type='button' id-objeto='" + item.id + "' class='btn btn-danger'><span class='glyphicon glyphicon glyphicon-remove' aria-hidden='true'></span></button>";
-                    template += "</div>";
-                    template += "</td>";
+                        var template = "<td>";
+                        template += "<div class='btn-group btn-group-xs'>";
+                        template += "<button type='button' id-objeto='" + item.id + "' class='btn btn-default'><span class='glyphicon glyphicon glyphicon-pencil' aria-hidden='true'></span></button>";
+                        template += "<button type='button' id-objeto='" + item.id + "' class='btn btn-danger'><span class='glyphicon glyphicon glyphicon-remove' aria-hidden='true'></span></button>";
+                        template += "</div>";
+                        template += "</td>";
 
-                    tr.append(template);
+                        tr.append(template);
 
-                    $('.table-result').append(tr);
-                });
-
-                adicionarEventos();
-
+                        $('.table-result').append(tr);
+                    });
+                    adicionarEventos();
+                    
+                }
             },
             error: function (xhr) {
                 alert("Ocorreu um erro ao carregar a lista.");
@@ -136,9 +142,11 @@ var Tipos = function () {
     return {
         //Função principal que inicializa o módulo
         inicializar: function () {
+
             $('#btn-cadastrar').click(cadastrarItem);
+
             carregarLista();
-            Utils.limparLista();
+
         }
     };
 } ();
